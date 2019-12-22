9477bd9dc1d5a1f7a173cdf533f015badb45ac4f
ugur ozoz <ugurozoz@yahoo.com>
2019-12-22 23:22:14 +0300

    some css,html tasks, gulp settings, and VJS

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,105 @@
-const { src, dest,parallel,watch,series } = require('gulp');
-const uglify = require('gulp-uglify')
-const livereload = require('gulp-livereload')
-const concat = require('gulp-concat')
-const plumber = require('gulp-plumber')
-const sourcemaps = require('gulp-sourcemaps')
-const sass = require('gulp-sass')
-const babel = require('gulp-babel')
-const browserSync = require('browser-sync').create();
+const { src, dest, parallel, watch, series } = require("gulp");
+const uglify = require("gulp-uglify");
+const livereload = require("gulp-livereload");
+const concat = require("gulp-concat");
+const plumber = require("gulp-plumber");
+const sourcemaps = require("gulp-sourcemaps");
+const sass = require("gulp-sass");
+const babel = require("gulp-babel");
+const browserSync = require("browser-sync").create();
 
-
-
-
-var gulp = require('gulp')
+var gulp = require("gulp");
 
 // File paths
-var DIST_PATH = 'public'
-var SCRIPTS_PATH = 'scripts/**/*.js'
-var SASS_PATH = 'sass/**/*.scss'
-var HTML_PATH = 'index.html'
-var IMAGES_PATH = 'images/**/*.{png,jpeg,jpg,svg,gif}'
+var DIST_PATH = "public";
+var SCRIPTS_PATH = "scripts/**/*.js";
+var SASS_PATH = "sass/**/*.scss";
+var HTML_PATH = "index.html";
+var IMAGES_PATH = "images/**/*.{png,jpeg,jpg,svg,gif}";
 
 //Image Compression
-var imagemin = require('gulp-imagemin')
-var imageminPngquant = require('imagemin-pngquant')
-var imageminJpegRecompress = require('imagemin-jpeg-recompress')
+var imagemin = require("gulp-imagemin");
+var imageminPngquant = require("imagemin-pngquant");
+var imageminJpegRecompress = require("imagemin-jpeg-recompress");
 
-
-
-console.log('>>>>>',__dirname)
+console.log(">>>>>", __dirname);
 
 function compileSass(cb) {
   src(SASS_PATH) // Gets all files ending with .scss in app/scss and children dirs
     .pipe(plumber())
-    .pipe(sass({
-      includePaths: ["scss"].concat()
-  }))
-    .pipe(dest('public/css'))
-    .pipe(livereload())
-    cb();
+    .pipe(
+      sass({
+        includePaths: ["scss"].concat()
+      })
+    )
+    .pipe(dest("public/css"))
+    .pipe(livereload());
+  cb();
 }
 
 // Scripts
 
-function scripts(cb) {  
+function scripts(cb) {
   src(SCRIPTS_PATH)
-      .pipe(plumber(function(err){
-          console.log('scripts task error')
-          console.log(err)
-          this.emit('end');
-      }))
-      .pipe(sourcemaps.init())
-      .pipe(babel({
-        presets: ['@babel/env']
-      }))
-      .pipe(uglify())
-      .pipe(concat('appXHR.js'))
-      .pipe(sourcemaps.write())
-      .pipe(dest(DIST_PATH+'/js'))
-    .pipe(livereload())
-    cb();
+    .pipe(
+      plumber(function(err) {
+        console.log("scripts task error");
+        console.log(err);
+        this.emit("end");
+      })
+    )
+    .pipe(sourcemaps.init())
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(uglify())
+    .pipe(concat("appXHR.js"))
+    .pipe(sourcemaps.write())
+    .pipe(dest(DIST_PATH + "/js"))
+    .pipe(livereload());
+  cb();
 }
 
 // Images
 
-function images(cb)  {
+function images(cb) {
   src(IMAGES_PATH)
-  .pipe(imagemin(
-      [
-          imagemin.gifsicle(),
-          imagemin.jpegtran(),
-          imagemin.optipng(),
-          imagemin.svgo(),
-          imageminPngquant(),
-          imageminJpegRecompress()
-      ]
-  ))
-    .pipe(dest(DIST_PATH + '/images'))
-    cb();
+    .pipe(
+      imagemin([
+        imagemin.gifsicle(),
+        imagemin.jpegtran(),
+        imagemin.optipng(),
+        imagemin.svgo(),
+        imageminPngquant(),
+        imageminJpegRecompress()
+      ])
+    )
+    .pipe(dest(DIST_PATH + "/images"));
+  cb();
 }
-
-
 
 // copies only index html
 function html(cb) {
-  gulp.src(HTML_PATH)
-    .pipe(dest('public/'))
+  gulp
+    .src(HTML_PATH)
+    .pipe(dest("public/"))
     .pipe(livereload());
-    cb();
-    
+  cb();
 }
 
 //Watch
 function watcher(cb) {
-  watch(`index.html`).on('change', series(html, browserSync.reload))
-  watch(`${SASS_PATH}`).on('change', series(compileSass, browserSync.reload))
-  watch(`${SCRIPTS_PATH}`).on('change', series(scripts, browserSync.reload))
+  watch(`index.html`).on("change", series(html, browserSync.reload));
+  watch(`${SASS_PATH}`).on("change", series(compileSass, browserSync.reload));
+  watch(`${SCRIPTS_PATH}`).on("change", series(scripts, browserSync.reload));
+  watch(`${IMAGES_PATH}`, { events: "all" }, function imgwatch() {
+    series(images, browserSync.reload);
+  });
+
   cb();
 }
-
 
 function server(cb) {
   browserSync.init({
@@ -105,12 +107,14 @@ function server(cb) {
     open: false,
     server: {
       baseDir: DIST_PATH
-    }   
-  })
+    }
+  });
   cb();
 }
 
-
-
 // Default
-exports.default = series(parallel(compileSass,images,scripts,html),server,watcher)
+exports.default = series(
+  parallel(compileSass, images, scripts, html),
+  server,
+  watcher
+);
--- scripts/appXHR.js
+++ scripts/appXHR.js
@@ -1,63 +1,149 @@
-// Search Input 
-const src = document.getElementById('github-search')
-const userphoto = document.querySelector('.github__photo')
+// Search Input
+"use strict";
 
-
-
-
-
+const src = document.getElementById("github-search");
+const userphoto = document.querySelector(".github__photo");
+const githubprofile = document.querySelector(".github__profile-button a");
+const githubstats = document.querySelector(".github__right__top");
+const githubnfo = document.querySelector(".github__right__mid");
+const githubrepos = document.querySelector(".github__repos");
 function githubFinder() {
-  this.xhr = new XMLHttpRequest()
+  this.xhr = new XMLHttpRequest();
 }
 
+githubFinder.prototype.findUser = function(usr, callback) {
+  const url = `https://api.github.com/users/${usr}`;
+  this.xhr.open("GET", url, true);
 
-
-githubFinder.prototype.findUser = function(usr, callback) {  
-  const url =`https://api.github.com/users/${usr}`
-  this.xhr.open('GET', url, true)
-
-  let self = this  
-  this.xhr.onload = function () {
+  let self = this;
+  this.xhr.onload = function() {
     if (self.xhr.status === 200) {
-      callback(null,self.xhr.responseText)
-      
+      callback(null, self.xhr.responseText);
     } else {
-      callback('Error : '+self.xhr.status)
+      callback("Error : " + self.xhr.status);
     }
+  };
+  this.xhr.send();
+};
+
+const setRepos = function(repos) {
+  clear(githubrepos);
+  console.log(">>", repos);
+
+  repos.sort((a, b) => b.id - a.id);
+  for (let i = 0; i < 4; i++) {
+    const repodiv = document.createElement("div");
+    repodiv.className = "github__repo";
+
+    const repoLink = document.createElement("a");
+    repoLink.textContent = repos[i].name;
+    repoLink.setAttribute("href", repos[i].html_url);
+    repoLink.setAttribute("target", "_blank");
+
+    const repoStars = document.createElement("span");
+    repoStars.textContent = `Stars: ${repos[i].stargazers_count}`;
+
+    const repoWatchers = document.createElement("span");
+    repoWatchers.textContent = `Watchers: ${repos[i].watchers_count}`;
+
+    const repoForks = document.createElement("span");
+    repoForks.textContent = `Forks: ${repos[i].forks_count}`;
+
+    repodiv.appendChild(repoLink);
+    repodiv.appendChild(repoStars);
+    repodiv.appendChild(repoWatchers);
+    repodiv.appendChild(repoForks);
+    githubrepos.appendChild(repodiv);
   }
-  this.xhr.send()    
-}
 
+  //githubrepos;
+};
 
-githubFinder.prototype.getUserInfo = function (usr) { }
+githubFinder.prototype.getUserInfo = function(usr) {
+  clear(userphoto);
+  console.log(usr);
+  // PROFILE LINK
+  const userPhotoLink = usr.avatar_url;
+  const profileLink = usr.html_url;
+  githubprofile.setAttribute("href", profileLink);
 
+  // PROFILE PHOTO
+  this.setPhoto(userPhotoLink);
 
+  //STATS
+  this.setStats(
+    usr.public_repos,
+    usr.public_gists,
+    usr.followers,
+    usr.following
+  );
 
-
-const finder = new githubFinder()
-
-
-
-
-
-
-document.addEventListener('keyup', () => {  
-  finder.findUser(src.value, function (err, res) {
+  this.setNfo(usr.company, usr.blog, usr.location, usr.created_at);
+  this.findRepos(usr.login, function(err, res) {
+    //console.clear();
     if (err) {
-      console.log(err)      
+      console.log(err);
     } else {
-      console.log(res)      
-    }    
-  })
-})
+      setRepos(JSON.parse(res));
+    }
+  });
+};
 
+githubFinder.prototype.findRepos = function(usr, callback) {
+  const url = `https://api.github.com/users/${usr}/repos`;
+  this.xhr.open("GET", url, true);
 
+  let self = this;
+  this.xhr.onload = function() {
+    if (self.xhr.status === 200) {
+      callback(null, self.xhr.responseText);
+    } else {
+      callback("Error : " + self.xhr.status);
+    }
+  };
+  this.xhr.send();
+};
 
+const clear = function(node) {
+  while (node.firstChild) {
+    node.removeChild(node.firstChild);
+  }
+  //userphoto.removeChild(userphoto.firstChild);
+};
 
+githubFinder.prototype.setPhoto = function(userPhotoLink) {
+  const userImg = document.createElement("img");
+  userImg.setAttribute("src", userPhotoLink);
+  userphoto.appendChild(userImg);
+};
 
+githubFinder.prototype.setStats = function(pr, pg, fs, fg) {
+  //Public repos, public gists, followers, following
+  const userstats = githubstats.querySelectorAll(".github__stat span");
+  userstats[0].textContent = pr ? pr : 0;
+  userstats[1].textContent = pg ? pg : 0;
+  userstats[2].textContent = fs ? fs : 0;
+  userstats[3].textContent = fg ? fg : 0;
+};
 
+githubFinder.prototype.setNfo = function(company, website, location, memdate) {
+  const userNfo = githubnfo.querySelectorAll(".github__nfo span");
+  userNfo[0].textContent = company;
+  userNfo[1].textContent = website;
+  userNfo[2].textContent = location;
+  userNfo[3].textContent = memdate;
+};
 
+const finder = new githubFinder();
+finder.setStats();
 
-
-
-
+document.addEventListener("keyup", () => {
+  finder.findUser(src.value, function(err, res) {
+    //console.clear();
+    if (err) {
+      console.log(err);
+    } else {
+      finder.getUserInfo(JSON.parse(res));
+    }
+  });
+});
